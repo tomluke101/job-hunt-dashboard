382f510f80069151f024553b2e0a3a4fddf7525d
Tom Luke <tomluke101@gmail.com>
2026-04-18 14:49:24 +0100

    Simplify Supabase auth, add all env vars

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,22 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-import { auth } from "@clerk/nextjs/server";
 
-export async function createServerSupabaseClient() {
-  const { getToken } = await auth();
-  const token = await getToken({ template: "supabase" });
-
-  return createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      global: {
-        headers: { Authorization: `Bearer ${token}` },
-      },
-    }
-  );
-}
-
-export function createServiceSupabaseClient() {
+export function createServerSupabaseClient() {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!
